ffb01c6bb26ee724714b3abb9a2f0761051c7fc0
dlwngh1367 <dlwngh1367@gmail.com>
2022-06-23 12:40:22 -0400

    8th commit: Daily Java - Constructors Part2

--- Bank Account/src/Main_BankAccount.java
+++ Bank Account/src/Main_BankAccount.java
@@ -7,33 +7,33 @@ public class Main_BankAccount {
 		System.out.println(juhosAccount.getNumber());
 		System.out.println(juhosAccount.getBalance());
 		
-	//	juhosAccount.setNumber("6777-12837-123");
-	//	juhosAccount.setBalance(0.00);
-	//	juhosAccount.setCustomername("Juho Lee");
-	//	juhosAccount.setCustomerEmailAddress("juholee@juho.com");
-	//	juhosAccount.setCustomerPhoneNumber("(613) 123-4567");
+		juhosAccount.setNumber("6777-12837-123");
+		juhosAccount.setBalance(0.00);
+		juhosAccount.setCustomerName("Juho Lee");
+		juhosAccount.setCustomerEmailAddress("juholee@juho.com");
+		juhosAccount.setCustomerPhoneNumber("(613) 123-4567");
 				
-//		juhosAccount.withdrawal(100.0);
-//		
-//		juhosAccount.deposit(50.0);
-//		juhosAccount.withdrawal(100.0);
-//		
-//		juhosAccount.deposit(51.0);
-//		juhosAccount.withdrawal(100.0);
-//		
-		Account timsAccount = new Account("Tim", "time@email.com", "12345");
-		System.out.println(timsAccount.getNumber() + "Name" + timsAccount.getCustomerName());
-		System.out.println("Current balance is " + timsAccount.getBalance());
-		timsAccount.withdrawal(100.55);
+		juhosAccount.withdrawal(100.0);
 		
-//		VipPerson person1 = new VipPerson();
-//		System.out.println(person1.getName());
-//		
-//		VipPerson person2 = new VipPerson("Bob", 25000.00);
-//		System.out.println(person2.getName());
-//		VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
-//		System.out.println(person3.getName());
-//		System.out.println(person3.getEmailAddress());
+		juhosAccount.deposit(50.0);
+		juhosAccount.withdrawal(100.0);
+		
+		juhosAccount.deposit(51.0);
+		juhosAccount.withdrawal(100.0);
+		
+		Account JuhosAccount2 = new Account("Juho", "Juho@email.com", "12345");
+		System.out.println(JuhosAccount2.getNumber() + " Name: " + JuhosAccount2.getCustomerName());
+		System.out.println("Current balance is " + JuhosAccount2.getBalance());
+		JuhosAccount2.withdrawal(100.55);
+		
+		VipPerson person1 = new VipPerson();
+		System.out.println(person1.getName());
+		
+		VipPerson person2 = new VipPerson("Bob", 25000.00);
+		System.out.println(person2.getName());
+		VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
+		System.out.println(person3.getName());
+		System.out.println(person3.getEmailAddress());
 	}
 	
 
