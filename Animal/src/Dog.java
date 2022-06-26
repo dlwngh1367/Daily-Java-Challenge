f3f4ffc7b5ecf2b1cd8d133e20d6d7e023db4f06
dlwngh1367 <dlwngh1367@gmail.com>
2022-06-26 00:51:02 -0400

    10th commit: Daily Java - Inheritance Part2

--- Animal/src/Dog.java
+++ Animal/src/Dog.java
@@ -25,5 +25,25 @@ public class Dog extends Animal {
 		System.out.println("Dog.chew() called");
 		chew();
 		super.eat();
-	}	
+	}
+	public void walk() {
+		System.out.println("dog.walk() called");
+		super.move(5);
+	}
+		
+	public void run() {
+		System.out.println("dog.run() called");
+		move(10);
+	
+	}
+	private void moveLegs(int speed) {
+		System.out.println("Dog.moveLegs() called");
+	}
+	
+	public void move(int speed) {
+		System.out.println("Dog.move() called");
+		moveLegs(speed);
+		super.move(speed);
+		
+	}
 }
